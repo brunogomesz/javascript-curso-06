e68b8aacbddbc25f4a7abaa08fa61b6f3b38d258
brunogomesz@hotmail.com <62654168+brunogomesz@users.noreply.github.com>
2023-02-15 15:37:20 -0300

    fim do curso 06

--- src/cli.js
+++ src/cli.js
@@ -1,38 +1,47 @@
 import chalk from 'chalk';
 import fs from 'fs';
 import pegaArquivo from './index.js';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
-function imprimeLista(resultado, identificador = '') {
-  console.log(
-    chalk.yellow('lista de links'),
-    chalk.black.bgGreen(identificador),
-    resultado);
+async function imprimeLista(valida, resultado, identificador = '') {
+  if (valida) {
+    console.log(
+      chalk.yellow('lista validada'),
+      chalk.black.bgGreen(identificador),
+      await listaValidada(resultado));    
+  } else {
+    console.log(
+      chalk.yellow('lista de links'),
+      chalk.black.bgGreen(identificador),
+      resultado);
+  }
 }
 
-async function processaTexto (argumentos) {
+
+async function processaTexto(argumentos) {
   const caminho = argumentos[2];
+  const valida = argumentos[3] === '--valida';
 
   try {
     fs.lstatSync(caminho);
   } catch (erro) {
-    if (erro.code === 'ENOENT') {
+    if (erro.code === 'ENOENT') { // está tratando o erro ENOENT e escrevendo algo mais fácil de compreender
       console.log('arquivo ou diretório não existe');
       return;
     }
   }
-
   //verifica se é um caminho de um diretório ou se é um caminho de arquivo específico
   if (fs.lstatSync(caminho).isFile()) {
     const resultado = await pegaArquivo(argumentos[2]);
-    imprimeLista(resultado);
+    imprimeLista(valida, resultado);
   } else if (fs.lstatSync(caminho).isDirectory()) {
-    const arquivos = await fs.promises.readdir(caminho)
+    const arquivos = await fs.promises.readdir(caminho) // lê os diretórios do caminho passado
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-      imprimeLista(lista, nomeDeArquivo);
-      })
+      imprimeLista(valida, lista, nomeDeArquivo)
+    })
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
   const capturas = [...texto.matchAll(regex)];
-  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]})); // 
 
   return resultados.length !== 0 ? resultados : 'não há links no arquivo';
   /*   Abaixo de const capturas = [...texto.matchAll(regex)]; vamos usar o método map dentro de uma nova constante resultados, que serve para percorrer um array e retornar outro array com o resultado que queremos. Vamos informar o objeto que queremos montar após uma arrow function.
@@ -22,8 +22,8 @@ function trataErro(erro) {
 async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
-    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    return extraiLinks(texto);
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding) // lê o arquivo e joga na variável textp
+    return extraiLinks(texto); // extrai os links e os títulos da string texto
   } catch (erro) {
     trataErro(erro)
   } finally {
